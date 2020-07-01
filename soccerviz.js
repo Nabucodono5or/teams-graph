e2ad47ba9fb0cf2f520f80adadc59f95ac9c56c0
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-07-01 11:22:03 -0300

    ainda em processo de construcao do evento click

--- soccerviz.js
+++ soccerviz.js
@@ -1,8 +1,12 @@
 import { csv } from "d3-fetch";
 import { select, selectAll } from "d3-selection";
-import moduleName from 'd3';
+import { keys } from "d3-collection";
+import { max } from "d3-array";
 
 function overallTeamViz(incomingData, tagRaiz) {
+  let dataKeys = criandoKeys(incomingData[0]);
+  let tag = "button.teams";
+
   select(tagRaiz)
     .append("g")
     .attr("id", "teamsG")
@@ -33,11 +37,42 @@ function overallTeamViz(incomingData, tagRaiz) {
     .text((d) => {
       return d.team;
     });
+
+  function buttonClick(datapoint) {
+    console.log("foi cricado");
+
+    //   let maxValue = max(incomingData, (el) => {
+    //     return parseFloat(el[datapoint]);
+    //   });
+  }
+
+  criandoButtons(dataKeys);
+  criandoEventoClick(tag, buttonClick);
 }
 
-// function criandoKeys() {
-//     let dataKeys = 
-// }
+function criandoEventoClick(tag, callback) {
+  selectAll(tag).on("click", callback);
+}
+
+function criandoButtons(dataKeys) {
+  select("#controls")
+    .selectAll("button.teams")
+    .data(dataKeys)
+    .enter()
+    .append("button")
+    .attr("class", "teams")
+    .html((d) => {
+      return d;
+    });
+}
+
+function criandoKeys(objeto) {
+  let dataKeys = keys(objeto).filter((propierty) => {
+    return propierty != "team" && propierty != "region";
+  });
+
+  return dataKeys;
+}
 
 function createSoccerViz() {
   csv(require("./data/worldcup.csv")).then((data) => {
