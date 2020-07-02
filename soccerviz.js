a09f0da7343f7c687a8fb5bbd80a44ea21ee2e83
Jefferson Abreu <pereirajefferson06@gmail.com>
2020-07-02 08:40:01 -0300

    adicionando comentários

--- soccerviz.js
+++ soccerviz.js
@@ -4,6 +4,59 @@ import { keys } from "d3-collection";
 import { max } from "d3-array";
 import { scaleLinear } from "d3";
 
+/// ----------------funções auxiliares --------------------------------
+function inciandoGraficos(teamG) {
+  teamG
+    .append("circle")
+    .attr("r", 0)
+    .transition()
+    .delay((d, i) => {
+      return i * 100;
+    })
+    .transition()
+    .duration(500)
+    .attr("r", 40)
+    .transition()
+    .duration(500)
+    .attr("r", 20)
+    .style("fill", "pink")
+    .style("stroke", "black")
+    .style("stroke-width", "1px");
+}
+
+function criandoEventoMouseOver(tag, callback) {
+  selectAll(tag).on("mouseover", callback);
+}
+
+function criandoEventoClick(tag, callback) {
+  selectAll(tag).on("click", callback);
+}
+
+function criandoEventoMouseout(tag, callback) {
+  selectAll(tag).on("mouseout", callback);
+}
+
+function criandoButtons(dataKeys) {
+  select("#controls")
+    .selectAll("button.teams")
+    .data(dataKeys)
+    .enter()
+    .append("button")
+    .attr("class", "teams")
+    .html((d) => {
+      return d;
+    });
+}
+
+function criandoKeys(objeto) {
+  let dataKeys = keys(objeto).filter((propierty) => {
+    return propierty != "team" && propierty != "region";
+  });
+
+  return dataKeys;
+}
+
+//--------------------------- função principal -----------------------
 function overallTeamViz(incomingData, tagRaiz) {
   let dataKeys = criandoKeys(incomingData[0]);
 
@@ -75,57 +128,7 @@ function overallTeamViz(incomingData, tagRaiz) {
   criandoEventoMouseout("g.overallG", eventMouseout);
 }
 
-function inciandoGraficos(teamG) {
-  teamG
-    .append("circle")
-    .attr("r", 0)
-    .transition()
-    .delay((d, i) => {
-      return i * 100;
-    })
-    .transition()
-    .duration(500)
-    .attr("r", 40)
-    .transition()
-    .duration(500)
-    .attr("r", 20)
-    .style("fill", "pink")
-    .style("stroke", "black")
-    .style("stroke-width", "1px");
-}
-
-function criandoEventoMouseOver(tag, callback) {
-  selectAll(tag).on("mouseover", callback);
-}
-
-function criandoEventoClick(tag, callback) {
-  selectAll(tag).on("click", callback);
-}
-
-function criandoEventoMouseout(tag, callback) {
-  selectAll(tag).on("mouseout", callback);
-}
-
-function criandoButtons(dataKeys) {
-  select("#controls")
-    .selectAll("button.teams")
-    .data(dataKeys)
-    .enter()
-    .append("button")
-    .attr("class", "teams")
-    .html((d) => {
-      return d;
-    });
-}
-
-function criandoKeys(objeto) {
-  let dataKeys = keys(objeto).filter((propierty) => {
-    return propierty != "team" && propierty != "region";
-  });
-
-  return dataKeys;
-}
-
+//----------------------- loading dos dados -----------------------------
 function createSoccerViz() {
   csv(require("./data/worldcup.csv")).then((data) => {
     console.log(data);
